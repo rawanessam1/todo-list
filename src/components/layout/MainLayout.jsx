065ed9dc73d15d4ef6216ba6e0f057c752e4d0f9
Rawan Essam <rawanessamm1@gmail.com>
2025-08-23 19:06:13 +0300

    edited SidebarMenu

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -3,7 +3,7 @@ import SidebarMenu from "./SidebarMenu";
 
 const MainLayout = ({ children, bgcColor }) => {
   return (
-    <div className="flex h-screen">
+  <div className="container mx-auto flex h-screen gap-8 pt-6 pb-6">
       <SidebarMenu />
       <div className={`flex-1 p-6 rounded-3xl shadow-md mx-6 overflow-auto ${bgcColor} `}>
         {children}
--- src/components/layout/SidebarMenu.jsx
+++ src/components/layout/SidebarMenu.jsx
@@ -11,48 +11,52 @@ import { FiLogOut } from "react-icons/fi";
 
 const SidebarMenu = () => {
   return (
-    <div className="w-64 bg-color-lightGray2 shadow-md rounded-3xl p-6 flex flex-col justify-between">
+    <div className="w-80 bg-lightGray2 shadow-md rounded-40 p-12 flex flex-col justify-between">
       <div>
         <div className="flex justify-between items-center mb-4">
-          <h2 className="text-2xl font-bold">Menu</h2>
+          <h2 className="text-4xl font-bold font-oswald">Menu</h2>
           <MenuOutlined className="text-xl" />
         </div>
-        <Input prefix={<SearchOutlined />} placeholder="Search..." className="rounded-full mb-6"/>
-        <div className="mb-6">
-          <h3 className="font-semibold mb-3">Tasks</h3>
-          <ul className="space-y-3">                    {/* for space between children */}
-            <li className="flex justify-between">
-              <span><MdKeyboardDoubleArrowRight /> Upcoming</span>
-              <span className="bg-gray-200 text-xs px-2 py-1 rounded-full">15+</span>
+        <Input prefix={<SearchOutlined />} placeholder="Search..." className="!bg-lightGray !rounded-full mb-6 !border-0 focus:!outline-none focus:!shadow-none"/>
+        <div className="mb-6 text-sm">
+          <h3 className="font-semibold mb-3 font-oswald">Tasks</h3>
+          <ul className="space-y-3 justify-end space-x-4">                    {/* for space between children */}
+            <li className="flex justify-between pl-6">
+              <div className="flex gap-2"><MdKeyboardDoubleArrowRight /><span> Upcoming</span></div>
+              <span className="bg-lightGray text-xs p-2 rounded-full">15+</span>
             </li>
-            <li className="flex justify-between">
-              <span><TfiMenuAlt /> Today</span>
-              <span className="bg-gray-200 text-xs px-2 py-1 rounded-full">8</span>
+            <li className="flex justify-between pl-6">
+              <div className="flex gap-2"><TfiMenuAlt /><span> Today</span></div>
+              <span className="bg-lightGray text-xs p-2 rounded-full">8</span>
             </li>
-            <li><LuCalendar /> Calendar</li>
-            <li><FaStickyNote /> Sticky Wall</li>
+            <li className="pl-6">
+                <div className="flex gap-2"><LuCalendar /> <span> Calendar</span></div>
+            </li>
+            <li className="pl-6">
+                <div className="flex gap-2"><FaStickyNote /> <span>Sticky Wall</span></div>
+            </li>
           </ul>
         </div>
-        <div>
-          <h3 className="font-semibold mb-3">Lists</h3>
+        <div className="text-sm">
+          <h3 className="font-semibold mb-4 font-oswald">Lists</h3>
           <ul className="space-y-3">
-            <li className="flex items-center gap-3">
-              <span className="w-3 h-3 rounded-full bg-red-500"></span> Work
+            <li className="flex items-center gap-3 bg-lightGray pl-6 rounded-40 p-1">
+              <span className="w-4 h-3 rounded-full bg-red-500"></span> Work
             </li>
-            <li className="flex items-center gap-3">
-              <span className="w-3 h-3 rounded-full bg-green-500"></span> Personal
+            <li className="flex items-center gap-3 bg-lightGray pl-6 rounded-40 p-1">
+              <span className="w-4 h-3 rounded-full bg-green-500"></span> Personal
             </li>
-            <li className="flex items-center gap-3">
-              <span className="w-3 h-3 rounded-full bg-blue-500"></span> Study
+            <li className="flex items-center gap-3 bg-lightGray pl-6 rounded-40 p-1">
+              <span className="w-4 h-3 rounded-full bg-blue-500"></span> Study
             </li>
-            <li className="text-gray-500 flex items-center gap-2"><IoIosAddCircleOutline /> Add new list</li>
+            <li className="text-black/70 flex items-center gap-2 pl-6"><IoIosAddCircleOutline /> Add new list</li>
           </ul>
         </div>
       </div>
 
-      <div className="space-y-3">
-        <div className="flex items-center gap-2"><RiMenu5Fill /> Settings</div>
-        <div className="flex items-center gap-2"><FiLogOut /> Sign Out</div>
+      <div className="space-y-3 text-lg mt-12 text-black/70">
+        <div className="flex gap-2"><RiMenu5Fill /> Settings</div>
+        <div className="flex gap-2"><FiLogOut /> Sign Out</div>
       </div>
     </div>
   );
